ff1143072dede705f506c65d7ee96993e48c5fb1
xiaolu_fan <15230093114@163.com>
2023-06-26 00:23:48 +0800

    fourth_day

--- main.cpp
+++ main.cpp
@@ -5,4 +5,5 @@ int main(int agrc, char **agrv)
 	std::cout << "first_day" << std::endl;
 	std::cout << "second_day" << std::endl;
 	std::cout << "third_day" << std::endl;
+	std::cout << "fourth_day" << std::endl;
 }
