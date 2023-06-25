f6eee898dc7ad937819e2c8ee516a5ee451c95f1
xiaolu-fan <15230093114@163.com>
2023-06-26 00:20:46 +0800

    third_day

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "iostream"
 
-int main(int agrc,char** agrv){
-	std::cout<<"first_day"<<std::endl;
-	std::cout<<"second_day"<<std::endl;
+int main(int agrc, char **agrv)
+{
+	std::cout << "first_day" << std::endl;
+	std::cout << "second_day" << std::endl;
+	std::cout << "third_day" << std::endl;
 }
