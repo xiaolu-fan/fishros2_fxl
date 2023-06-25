e4ee23e3afae600b6d499d74b5a91aabf6675961
xiaolu-fan <15230093114@163.com>
2023-06-26 00:28:06 +0800

    delete fourth_day

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main(int agrc, char **agrv)
 	std::cout << "first_day" << std::endl;
 	std::cout << "second_day" << std::endl;
 	std::cout << "third_day" << std::endl;
-	std::cout << "fourth_day" << std::endl;
+
+	std::cout << "fifth_day" << std::endl;
 }
